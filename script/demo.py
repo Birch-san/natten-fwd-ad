c10cda502ef0340a25fc63873906309339e38488
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-24 21:49:58 +0000

    oops

--- script/demo.py
+++ script/demo.py
@@ -10,9 +10,9 @@ d_model=128
 d_head=64
 kernel_size=13
 torch.manual_seed(seed)
-natten_block = NattenBlock(d_model, d_head=64, kernel_size=13).to(device=device, dtype=dtype)
+natten_block = NattenBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 torch.manual_seed(seed)
-hood_block = NeighbourhoodAttnBlock(d_model, d_head=64, kernel_size=13).to(device=device, dtype=dtype)
+hood_block = NeighbourhoodAttnBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
 canvas_len=32
 x = torch.randn([1, canvas_len, canvas_len, d_model], device=device, dtype=dtype)

6bca04df1e2eae576e6bd7788eba5c67b074a106
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-24 23:05:08 +0000

    forward-mode AD working with math attn

--- script/demo.py
+++ script/demo.py
@@ -1,5 +1,7 @@
 import torch
-from torch import inference_mode
+from torch import inference_mode, enable_grad
+import torch.autograd.forward_ad as fwAD
+from torch.backends.cuda import sdp_kernel
 from src.natten_block import NattenBlock
 from src.hood_attn_block import NeighbourhoodAttnBlock
 
@@ -14,8 +16,9 @@ natten_block = NattenBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(d
 torch.manual_seed(seed)
 hood_block = NeighbourhoodAttnBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
+batch=2
 canvas_len=32
-x = torch.randn([1, canvas_len, canvas_len, d_model], device=device, dtype=dtype)
+x = torch.randn([batch, canvas_len, canvas_len, d_model], device=device, dtype=dtype)
 
 with inference_mode():
   out_natt = natten_block(x)
@@ -26,3 +29,11 @@ rtol=1e-5
 atol=5e-3
 assert out_natt.allclose(out_hood, rtol=1e-5, atol=5e-3), "assertion failure indicates implementations are not equivalent"
 print(f'NATTEN output matched pure-PyTorch implementation to within atol={atol}, rtol={rtol}')
+
+tangent = torch.randn([batch, canvas_len, canvas_len, d_model], device=device, dtype=dtype)
+with fwAD.dual_level(), enable_grad(), sdp_kernel(enable_math=True, enable_flash=False, enable_mem_efficient=False):
+  dual_x = fwAD.make_dual(x, tangent)
+  # out_natt = natten_block(dual_x)
+  out_hood = hood_block(dual_x)
+  out_hood_prime = fwAD.unpack_dual(out_hood).tangent
+  pass

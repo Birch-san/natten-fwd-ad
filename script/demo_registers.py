be295395a5c537da9070c12a4f1f7d3299b3639b
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-08 22:47:57 +0000

    registers, probably

--- script/demo_registers.py
+++ script/demo_registers.py
@@ -1,7 +1,7 @@
 import torch
 from torch import inference_mode
 # from src.natten_block import NattenBlock
-from src.hood_attn_registers_block import NeighbourhoodAttnBlock
+from src.hood_attn_registers_block import NeighbourhoodAttnRegisterBlock
 
 device=torch.device('cuda')
 dtype=torch.bfloat16
@@ -12,15 +12,17 @@ kernel_size=13
 # torch.manual_seed(seed)
 # natten_block = NattenBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 torch.manual_seed(seed)
-hood_block = NeighbourhoodAttnBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
+hood_block = NeighbourhoodAttnRegisterBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
 batch=2
 canvas_len=32
+register_count=16
 x = torch.randn([batch, canvas_len, canvas_len, d_model], device=device, dtype=dtype)
+registers = torch.randn([register_count, d_model], device=device, dtype=dtype)
 
 with inference_mode():
-  out_hood = hood_block(x)
+  out_hood = hood_block(x, registers)
   # not yet supported by NATTEN
-  # out_natt = natten_block(x)
+  # out_natt = natten_block(x, registers)
 # assert out_natt.allclose(out_hood, rtol=1e-5, atol=5e-3), "assertion failure indicates implementations are not equivalent"
 # print(f'NATTEN output matched pure-PyTorch implementation to within atol={atol}, rtol={rtol}')
--- src/hood_attn_registers_block.py
+++ src/hood_attn_registers_block.py
@@ -1,9 +1,9 @@
 import torch
-from torch import FloatTensor, BoolTensor
+from torch import FloatTensor, BoolTensor, cat
 from torch.nn import Module, Linear
-from torch.nn.functional import scaled_dot_product_attention
+from torch.nn.functional import scaled_dot_product_attention, linear
 from einops import rearrange
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 class Dimensions(NamedTuple):
   height: int
@@ -36,7 +36,7 @@ def make_neighbourhood_mask(kernel_size: Dimensions, canvas_size: Dimensions, fl
 
   return mask
 
-class NeighbourhoodAttnBlock(Module):
+class NeighbourhoodAttnRegisterBlock(Module):
   def __init__(self, d_model: int, d_head: int, kernel_size: int):
     """
     Pure-PyTorch implementation of neighbourhood attention.
@@ -50,18 +50,42 @@ class NeighbourhoodAttnBlock(Module):
     self.d_head = d_head
     self.n_heads = d_model // d_head
     self.kernel_size = kernel_size
-    self.qkv_proj = Linear(d_model, d_model * 3, bias=False)
-    self.out_proj = Linear(d_model, d_model, bias=False)
+    self.d_model = d_model
+    self.qkv_proj = Linear(d_model, d_model * 3, bias=True)
+    self.out_proj = Linear(d_model, d_model, bias=True)
 
-  def forward(self, x: FloatTensor) -> FloatTensor:
+  def forward(self, x: FloatTensor, registers: FloatTensor = None) -> FloatTensor:
     _, h, w, _ = x.shape
     qkv = self.qkv_proj(x)
+
+    # this has nothing to do with registers, I'm just slicing a kv_proj out of qkv_proj
+    kv_proj_weight: FloatTensor = self.qkv_proj.weight[self.d_model:, :]
+    kv_proj_bias: Optional[FloatTensor] = None if self.qkv_proj.bias is None else self.qkv_proj.bias[self.d_model:]
+
+    # project the registers through kv_proj (a separate kv_proj would be fine too)
+    reg_kv: FloatTensor = linear(registers, kv_proj_weight, kv_proj_bias)
+    # permute head channels into a batch dim, and unbind k and v from the fused kv
+    reg_k, reg_v = rearrange(reg_kv, "r (t nh e) -> t 1 nh r e", t=2, e=self.d_head)
+    # construct an all-True mask that says "attend to all registers"
+    mask_reg = reg_k.new_ones(reg_k.size(-2), dtype=torch.bool)
+
     q, k, v = rearrange(qkv, "n h w (t nh e) -> t n nh (h w) e", t=3, e=self.d_head)
+
+    # concatenate registers along the sequence dimension
+    k_cat = cat([k, reg_k.expand(k.size(0), -1, -1, -1)], dim=-2)
+    v_cat = cat([v, reg_v.expand(v.size(0), -1, -1, -1)], dim=-2)
+
+    # make a neighbourhood mask for x
     kernel_size=Dimensions(self.kernel_size, self.kernel_size)
     canvas_size=Dimensions(h, w)
     mask: BoolTensor = make_neighbourhood_mask(kernel_size, canvas_size, flatten_to_1d=True, device=x.device)
-    mask = mask.unsqueeze(0).unsqueeze(0)
-    x = scaled_dot_product_attention(q, k, v, attn_mask=mask)
+    # query items should attend to their local neighbourhood *and* all registers
+    mask_cat = cat([mask, mask_reg.expand(mask.size(0), -1)], dim=-1)
+    # broadcast to all heads and batch items
+    mask_cat = mask_cat.expand(1, 1, -1, -1)
+
+    x = scaled_dot_product_attention(q, k_cat, v_cat, attn_mask=mask_cat)
+    
     x = rearrange(x, "n nh (h w) e -> n h w (nh e)", h=h, w=w, e=self.d_head)
     x = self.out_proj(x)
     return x

30ff09e9d2c26ff6e6f7041290613a7db5747824
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-12 01:19:58 +0000

    pure-PyTorch concat-and-split

--- script/demo_registers.py
+++ script/demo_registers.py
@@ -3,6 +3,7 @@ from torch import inference_mode, FloatTensor
 from src.natten_registers_block import NattenRegistersBlock
 from src.natten_concatsplit_registers_block import NattenConcatSplitRegistersBlock
 from src.hood_attn_registers_block import NeighbourhoodAttnRegisterBlock
+from src.hood_attn_concatsplit_registers_block import NeighbourhoodAttnConcatSplitRegistersBlock
 
 device=torch.device('cuda')
 dtype=torch.bfloat16
@@ -16,6 +17,8 @@ torch.manual_seed(seed)
 natten_csplit_block = NattenConcatSplitRegistersBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 torch.manual_seed(seed)
 hood_block = NeighbourhoodAttnRegisterBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
+torch.manual_seed(seed)
+hood_split_block = NeighbourhoodAttnConcatSplitRegistersBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
 batch=2
 canvas_len=32
@@ -25,6 +28,7 @@ registers = torch.randn([register_count, d_model], device=device, dtype=dtype)
 
 with inference_mode():
   out_hood: FloatTensor = hood_block(x, registers)
+  out_hoodc: FloatTensor = hood_split_block(x, registers)
   out_natt: FloatTensor = natten_block(x, registers)
   out_nattc: FloatTensor = natten_csplit_block(x, registers)
 
@@ -32,6 +36,10 @@ with inference_mode():
 rtol=1e-5
 # this is actually a *wildly* generous atol. the default would normally be 1e-08.
 atol=5e-3
+assert out_natt.allclose(out_hoodc, rtol=rtol, atol=atol), "assertion failure indicates NATTEN built-in xattn does not match concat-and-split pure-PyTorch approach"
+print(f'NATTEN built-in xattn matched pure-PyTorch concat-and-split xattn to within atol={atol}, rtol={rtol}')
+assert out_hood.allclose(out_hoodc, rtol=rtol, atol=atol), "assertion failure indicates pure-PyTorch xattn does not match concat-and-split pure-PyTorch approach"
+print(f'pure-PyTorch xattn matched concat-and-split pure-PyTorch approach to within atol={atol}, rtol={rtol}')
 assert out_natt.allclose(out_nattc, rtol=rtol, atol=atol), "assertion failure indicates NATTEN built-in xattn does not match concat-and-split NATTEN approach"
 print(f'NATTEN built-in xattn matched concat-and-split NATTEN approach to within atol={atol}, rtol={rtol}')
 assert out_nattc.allclose(out_hood, rtol=rtol, atol=atol), "assertion failure indicates NATTEN built-in xattn does not match pure-PyTorch implementation"
--- src/hood_attn_concatsplit_registers_block.py
+++ src/hood_attn_concatsplit_registers_block.py
@@ -1,9 +1,9 @@
 import torch
-from torch import FloatTensor, BoolTensor
+from torch import FloatTensor, BoolTensor, cat, baddbmm
 from torch.nn import Module, Linear
-from torch.nn.functional import scaled_dot_product_attention
+from torch.nn.functional import linear
 from einops import rearrange
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 class Dimensions(NamedTuple):
   height: int
@@ -36,7 +36,7 @@ def make_neighbourhood_mask(kernel_size: Dimensions, canvas_size: Dimensions, fl
 
   return mask
 
-class NeighbourhoodAttnBlock(Module):
+class NeighbourhoodAttnConcatSplitRegistersBlock(Module):
   def __init__(self, d_model: int, d_head: int, kernel_size: int):
     """
     Pure-PyTorch implementation of neighbourhood attention.
@@ -47,21 +47,56 @@ class NeighbourhoodAttnBlock(Module):
     - Nested tensors
     """
     super().__init__()
+    self.d_model = d_model
     self.d_head = d_head
     self.n_heads = d_model // d_head
     self.kernel_size = kernel_size
     self.qkv_proj = Linear(d_model, d_model * 3, bias=False)
     self.out_proj = Linear(d_model, d_model, bias=False)
+    self.scale = d_head**-.5
 
-  def forward(self, x: FloatTensor) -> FloatTensor:
+  def forward(self, x: FloatTensor, registers: FloatTensor = None) -> FloatTensor:
     _, h, w, _ = x.shape
     qkv = self.qkv_proj(x)
+
+    # this has nothing to do with registers, I'm just slicing a kv_proj out of qkv_proj
+    kv_proj_weight: FloatTensor = self.qkv_proj.weight[self.d_model:, :]
+    kv_proj_bias: Optional[FloatTensor] = None if self.qkv_proj.bias is None else self.qkv_proj.bias[self.d_model:]
+
+    # project the registers through kv_proj (a separate kv_proj would be fine too)
+    reg_kv: FloatTensor = linear(registers, kv_proj_weight, kv_proj_bias)
+    # permute head channels into a batch dim, and unbind k and v from the fused kv
+    reg_k, reg_v = rearrange(reg_kv, "r (t nh e) -> t 1 nh r e", t=2, e=self.d_head)
+    # expand singleton batch dim to explicitly equal batch dim used in self-attn
+    reg_k, reg_v = (reg.expand(qkv.size(0), *reg.shape[1:]) for reg in (reg_k, reg_v))
+
     q, k, v = rearrange(qkv, "n h w (t nh e) -> t n nh (h w) e", t=3, e=self.d_head)
+    v_cat = cat([v, reg_v], dim=-2)
+
     kernel_size=Dimensions(self.kernel_size, self.kernel_size)
     canvas_size=Dimensions(h, w)
     mask: BoolTensor = make_neighbourhood_mask(kernel_size, canvas_size, flatten_to_1d=True, device=x.device)
-    mask = mask.unsqueeze(0).unsqueeze(0)
-    x = scaled_dot_product_attention(q, k, v, attn_mask=mask)
+    mask = mask.unsqueeze(0)
+
+    # batched matmul which fuses in the addition of the mask and the multiplication by scale factor
+    qk: FloatTensor = baddbmm(
+      (~mask).to(q.dtype),
+      q.flatten(end_dim=-3),
+      k.flatten(end_dim=-3).mT,
+      beta=-10_000,
+      alpha=self.scale,
+    ).unflatten(0, (-1, self.n_heads))
+    # this is just `q @ (reg_k.mT * self.scale)` except it fuses the scale factor in. hopefully faster?
+    qk_reg: FloatTensor = baddbmm(
+      q.new_zeros(1),
+      q.flatten(end_dim=-3),
+      reg_k.flatten(end_dim=-3).mT,
+      beta=0,
+      alpha=self.scale,
+    ).unflatten(0, (-1, self.n_heads))
+    qk_cat = cat([qk, qk_reg], dim=-1)
+    probs = qk_cat.softmax(dim=-1)
+    x = probs @ v_cat
     x = rearrange(x, "n nh (h w) e -> n h w (nh e)", h=h, w=w, e=self.d_head)
     x = self.out_proj(x)
     return x

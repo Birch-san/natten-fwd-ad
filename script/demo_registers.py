127649ef3511dbe9febcac641c242f111c39e8bc
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-12 00:54:04 +0000

    NATTEN concat-and-split approach

--- script/demo_registers.py
+++ script/demo_registers.py
@@ -1,6 +1,7 @@
 import torch
-from torch import inference_mode
+from torch import inference_mode, FloatTensor
 from src.natten_registers_block import NattenRegistersBlock
+from src.natten_concatsplit_registers_block import NattenConcatSplitRegistersBlock
 from src.hood_attn_registers_block import NeighbourhoodAttnRegisterBlock
 
 device=torch.device('cuda')
@@ -12,6 +13,8 @@ kernel_size=13
 torch.manual_seed(seed)
 natten_block = NattenRegistersBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 torch.manual_seed(seed)
+natten_csplit_block = NattenConcatSplitRegistersBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
+torch.manual_seed(seed)
 hood_block = NeighbourhoodAttnRegisterBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
 batch=2
@@ -21,12 +24,15 @@ x = torch.randn([batch, canvas_len, canvas_len, d_model], device=device, dtype=d
 registers = torch.randn([register_count, d_model], device=device, dtype=dtype)
 
 with inference_mode():
-  out_hood = hood_block(x, registers)
-  out_natt = natten_block(x, registers)
+  out_hood: FloatTensor = hood_block(x, registers)
+  out_natt: FloatTensor = natten_block(x, registers)
+  out_nattc: FloatTensor = natten_csplit_block(x, registers)
 
 # default rtol of allclose
 rtol=1e-5
 # this is actually a *wildly* generous atol. the default would normally be 1e-08.
 atol=5e-3
-assert out_natt.allclose(out_hood, rtol=rtol, atol=atol), "assertion failure indicates implementations are not equivalent"
+assert out_natt.allclose(out_nattc, rtol=rtol, atol=atol), "assertion failure indicates NATTEN built-in xattn does not match concat-and-split NATTEN approach"
+print(f'NATTEN built-in xattn matched concat-and-split NATTEN approach to within atol={atol}, rtol={rtol}')
+assert out_nattc.allclose(out_hood, rtol=rtol, atol=atol), "assertion failure indicates NATTEN built-in xattn does not match pure-PyTorch implementation"
 print(f'NATTEN output matched pure-PyTorch implementation to within atol={atol}, rtol={rtol}')
--- src/natten_concatsplit_registers_block.py
+++ src/natten_concatsplit_registers_block.py
@@ -1,29 +1,49 @@
-from torch import FloatTensor
+from torch import FloatTensor, cat
 from torch.nn import Module, Linear
+from torch.nn.functional import linear
 from einops import rearrange
+from typing import Optional
 
-# import natten.functional import natten2dqk, natten2dav
-from src.natten_autograd import natten2dqk, natten2dav
+from natten.functional import natten2dqk, natten2dav
 
 # Simplifed version of
 # Katherine Crowson's NeighborhoodSelfAttentionBlock, MIT license
 # https://github.com/crowsonkb/k-diffusion/blob/9737cfd85120cba1258b5b5b1dc6511356b5c924/k_diffusion/models/image_transformer_v2.py#L390
-class NattenBlock(Module):
+class NattenConcatSplitRegistersBlock(Module):
   def __init__(self, d_model: int, d_head: int, kernel_size: int):
     super().__init__()
+    self.d_model = d_model
     self.d_head = d_head
     self.n_heads = d_model // d_head
     self.kernel_size = kernel_size
     self.qkv_proj = Linear(d_model, d_model * 3, bias=False)
     self.out_proj = Linear(d_model, d_model, bias=False)
 
-  def forward(self, x: FloatTensor) -> FloatTensor:
+  def forward(self, x: FloatTensor, registers: FloatTensor = None) -> FloatTensor:
     qkv = self.qkv_proj(x)
+
+    # this has nothing to do with registers, I'm just slicing a kv_proj out of qkv_proj
+    kv_proj_weight: FloatTensor = self.qkv_proj.weight[self.d_model:, :]
+    kv_proj_bias: Optional[FloatTensor] = None if self.qkv_proj.bias is None else self.qkv_proj.bias[self.d_model:]
+
+    # project the registers through kv_proj (a separate kv_proj would be fine too)
+    reg_kv: FloatTensor = linear(registers, kv_proj_weight, kv_proj_bias)
+    # permute head channels into a batch dim, and unbind k and v from the fused kv
+    reg_k, reg_v = rearrange(reg_kv, "r (t nh e) -> t 1 nh r e", t=2, e=self.d_head)
+
     q, k, v = rearrange(qkv, "n h w (t nh e) -> t n nh h w e", t=3, e=self.d_head)
     q = q / self.d_head**.5
     qk = natten2dqk(q, k, self.kernel_size, 1)
-    a = qk.softmax(dim=-1)
-    x = natten2dav(a, v, self.kernel_size, 1)
+    # alternatively:
+    # qk_reg = (q.flatten(start_dim=-3, end_dim=-2) @ reg_k.mT).unflatten(-2, (x.size(-3), x.size(-2)))
+    qk_reg = q @ reg_k.mT.unsqueeze(-3)
+    qk_cat = cat([qk, qk_reg], dim=-1)
+    a = qk_cat.softmax(dim=-1)
+    a_neigh, a_reg = a.split_with_sizes((qk.size(-1), qk_reg.size(-1)), dim=-1)
+    attn_neigh = natten2dav(a_neigh, v, self.kernel_size, 1)
+    attn_reg = a_reg @ reg_v.unsqueeze(-3)
+    # is this right?
+    x = attn_neigh + attn_reg
     x = rearrange(x, "n nh h w e -> n h w (nh e)")
     x = self.out_proj(x)
     return x

c135012a3fa8cafee6aa67823e2cd27934552dbd
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-12 00:07:13 +0000

    implement Natten registers. not getting the same result though.

--- script/demo_registers.py
+++ script/demo_registers.py
@@ -1,6 +1,6 @@
 import torch
 from torch import inference_mode
-# from src.natten_block import NattenBlock
+from src.natten_registers_block import NattenRegistersBlock
 from src.hood_attn_registers_block import NeighbourhoodAttnRegisterBlock
 
 device=torch.device('cuda')
@@ -9,8 +9,8 @@ seed=42
 d_model=128
 d_head=64
 kernel_size=13
-# torch.manual_seed(seed)
-# natten_block = NattenBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
+torch.manual_seed(seed)
+natten_block = NattenRegistersBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 torch.manual_seed(seed)
 hood_block = NeighbourhoodAttnRegisterBlock(d_model, d_head=d_head, kernel_size=kernel_size).to(device=device, dtype=dtype)
 
@@ -22,7 +22,11 @@ registers = torch.randn([register_count, d_model], device=device, dtype=dtype)
 
 with inference_mode():
   out_hood = hood_block(x, registers)
-  # not yet supported by NATTEN
-  # out_natt = natten_block(x, registers)
-# assert out_natt.allclose(out_hood, rtol=1e-5, atol=5e-3), "assertion failure indicates implementations are not equivalent"
-# print(f'NATTEN output matched pure-PyTorch implementation to within atol={atol}, rtol={rtol}')
+  out_natt = natten_block(x, registers)
+
+# default rtol of allclose
+rtol=1e-5
+# this is actually a *wildly* generous atol. the default would normally be 1e-08.
+atol=5e-3
+assert out_natt.allclose(out_hood, rtol=rtol, atol=atol), "assertion failure indicates implementations are not equivalent"
+print(f'NATTEN output matched pure-PyTorch implementation to within atol={atol}, rtol={rtol}')
